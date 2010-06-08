58e9b35f616538866cd438ea3aec3a303adba4e1
Tim Song <rs2740@gmail.com>
2010-06-08 13:49:57 -0400

    Use betai for binomial CDF.

--- math.php
+++ math.php
@@ -1,5 +1,6 @@
 <?php
 
+define('FPMIN', '1.0e-30');
 /* 
  * Computes the factoral (x!).
  * @author Thomas Oldbury. 
@@ -68,6 +69,8 @@ function bcpowx($a, $b, $iters = 25, $scale = 100)
 //                    function', J. SIAM Numer. Anal., B, 1, 86-96, 1964.
 function lngamma($n)
 {
+	if($n == 0)
+		var_dump(debug_backtrace());
 	$temp = 0.9999999999995183;
 	$temp += 676.5203681218835/$n;
 	$temp -= 1259.139216722289/($n+1);
@@ -92,10 +95,54 @@ function binomial_pmf($n, $p, $k){
 }
 
 function binomial_cdf($n, $p, $k){
-	$ret = 0;
-	for($i = 0; $i <= $k; $i++)
-		$ret += binomial_pmf($n, $p, $i);
-	return $ret;
+	if($k >= $n) return 1;
+	return betai($n - $k, $k + 1, 1 - $p);
 }
 
+// Adapted from Numerical Recipes in C, 2e
+function betai($a, $b, $x){
+	if ($x < 0.0 || $x > 1.0) die("Invalid parameter to betai");
+	if ($x == 0.0 || $x == 1.0) $bt = 0.0;
+	else //Factors in front of the continued fraction.
+	$bt=exp(lngamma($a+$b)-lngamma($a)-lngamma($b)+$a*log($x)+$b*log(1.0-$x));
+	
+	if ($x < ($a+1.0)/($a+$b+2.0)) // Use continued fraction directly.
+		return $bt*betacf($a,$b,$x)/$a;
+	else //Use continued fraction after making the symmetry transformation
+		return 1.0-$bt*betacf($b,$a,1.0-$x)/$b;
+}
+
+//
+//Used by betai: Evaluates continued fraction for incomplete beta function.
+function betacf($a, $b, $x){
+	$qab=$a+$b;
+	$qap=$a+1.0;
+	$qam=$a-1.0;
+	$c=1.0; //First step of Lentz’s method.
+	$d=1.0-$qab*$x/$qap;
+	if (abs($d) < FPMIN) $d = FPMIN;
+	$d=1.0/$d;
+	$h=$d;
+	for ($m=1;$m<=100;$m++) {
+		$m2=2*$m;
+		$aa=$m*($b-$m)*$x/(($qam+$m2)*($a+$m2));
+		$d=1.0+$aa*$d;// One step (the even one) of the recurrence.
+		if (abs($d) < FPMIN) $d=FPMIN;
+		$c=1.0+$aa/$c;
+		if (abs($c) < FPMIN) $c=FPMIN;
+		$d=1.0/$d;
+		$h *= $d*$c;
+		$aa = -($a+$m)*($qab+$m)*$x/(($a+$m2)*($qap+$m2));
+		$d=1.0+$aa*$d; // Next step of the recurrence (the odd one).
+		if (abs($d) < FPMIN) $d=FPMIN;
+		$c=1.0+$aa/$c;
+		if (abs($c) < FPMIN) $c=FPMIN;
+		$d=1.0/$d;
+		$del=$d*$c;
+		$h *= $del;
+		if (abs($del-1.0) < 3.0e-7) break;
+	}
+	if ($m > 100) die("a or b too big, or MAXIT too small in betacf");
+	return $h;
+}
 ?>
