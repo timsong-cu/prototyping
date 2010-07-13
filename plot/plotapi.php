7921276f4b3ecbef8284740ce195fbe6833341ba
Tim Song <rs2740@gmail.com>
2010-07-12 22:16:17 -0400

    Fix small bug w/ negative binomial distribution

--- plot/plotapi.php
+++ plot/plotapi.php
@@ -42,9 +42,7 @@ if($calculation == 'distribution'){
 	}
 	else if($distribution == 'negativebinomial'){
 		$size = floatval($_REQUEST['size']);
-		$mean = floatval($_REQUEST['mu']);
 		if($size <= 0) $size = 1;
-		if($mean <= 0) $mean = 1;
 		$args = compact('size', 'mean', 'distribution');
 		$charttitle = "Depth-coverage distribution\n(Negative binomial, mean $mean, dispersion parameter $size)";
 		$params = implode('_', array($function, $mean, $size));
